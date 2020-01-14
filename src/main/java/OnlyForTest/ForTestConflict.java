b4365c5a0f556463972b5480c8ea2309d874d8fa
ftt <2327584078@qq.com>
2020-01-14 12:19:34 +0800

    fortest conflict

---
+++ src/main/java/OnlyForTest/ForTestConflict.java
@@ -0,0 +1,10 @@
+package OnlyForTest;
+
+public class ForTestConflict {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("only for test conflict");
+	}
+
+}
